dacb0cd1b6a803bccc08b2c0192320b0ae377534
Bay Phillips <bay.phillips@plated.com>
2018-09-06 14:54:51 -0400

    cleanup

--- src/nodes.js
+++ src/nodes.js
@@ -11,14 +11,9 @@ const {
 const JOB_POSTING_TYPE = `JobPosting`
 const DEPARTMENT_TYPE = `Department`
 
-export const JobPostingNode = createNodeFactory(JOB_POSTING_TYPE, node => {
-  console.log(`Created JobPostingNode with id ${node.id} and type ${node.type}`)
-  return node
-})
-
+export const JobPostingNode = createNodeFactory(JOB_POSTING_TYPE)
 
 export const DepartmentNode = createNodeFactory(DEPARTMENT_TYPE, node => {
-  console.log(`Created DepartmentNode with id ${node.id}`)
   node.children = node.jobPostings.map(jobPosting => {
     return generateNodeId(JOB_POSTING_TYPE, jobPosting.id)
   })
