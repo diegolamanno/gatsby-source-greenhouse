023f0b10ff271b91f23c50d53ac5d3a70c85cc16
Bay Phillips <bay.phillips@plated.com>
2018-09-10 19:35:28 -0400

    Add in support for Departments and viewing JobPosts for each department

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,19 +1,52 @@
 const crypto = require('crypto')
 const axios = require('axios')
+import { JobPostNode, DepartmentNode } from './nodes'
 
 /**
- * Greenhouse promise request.
+ * Return all open jobs for a given department
  * @param apiToken string.
- * @param queryParams object.
+ * @param departmentId string.
  */
-const getJobs = (apiToken, queryParams = { live: true }) =>
-	axios.get('https://harvest.greenhouse.io/v1/job_posts', {
+async function getJobsForDepartment(apiToken, departmentId) {
+	return axios.get('https://harvest.greenhouse.io/v1/jobs', {
+		params: {
+			department_id: departmentId,
+			status: 'open'
+		},
+		auth: {
+			username: apiToken,
+			password: '',
+		},
+	})
+}
+
+/**
+ * Return all job posts
+ * @param apiToken string.
+ * @param queryParams object, defaults to only live job posts
+ */
+async function getJobPosts(apiToken, queryParams) {
+	return axios.get('https://harvest.greenhouse.io/v1/job_posts', {
 		params: queryParams,
 		auth: {
 			username: apiToken,
 			password: '',
 		},
 	})
+}
+
+/**
+ * Gets all departments for a given organization
+ * @param apiToken string.
+ */
+async function getDepartments(apiToken) {
+	return axios.get('https://harvest.greenhouse.io/v1/departments', {
+		auth: {
+			username: apiToken,
+			password: '',
+		}
+	})
+}
 
 /**
  * Gatsby requires ID to be a string to define nodes and greenhouse.io uses an integer instead.
@@ -27,27 +60,69 @@ const changeId = obj => {
 	return updatedObj
 }
 
-exports.sourceNodes = async ({ boundActionCreators }, { apiToken, queryParams }) => {
+const defaultPluginOptions = {
+  jobPosts: {
+    live: true
+  }
+}
+
+exports.sourceNodes = async ({ boundActionCreators }, { apiToken, pluginOptions }) => {
 	const { createNode } = boundActionCreators
+  const options = pluginOptions || defaultPluginOptions
 
-	const result = await getJobs(apiToken, queryParams)
-	const jobs = result.data
+  console.log(`Fetch Greenhouse data`)
 
-	jobs.forEach(job => {
-		const jsonString = JSON.stringify(job)
-		const gatsbyNode = {
-			...changeId(job),
-			children: [],
-			parent: '__SOURCE__',
-			internal: {
-				type: 'GreenhouseJob',
-				content: jsonString,
-				contentDigest: crypto
-					.createHash('md5')
-					.update(jsonString)
-					.digest('hex'),
-			},
-		}
-		createNode(gatsbyNode)
-	})
+  console.log(`Starting to fetch data from Greenhouse`)
+
+  let departments, jobPosts
+  try {
+    departments = await getDepartments(apiToken).then(response => response.data)
+    jobPosts = await getJobPosts(apiToken, options.jobPosts).then(response => response.data)
+  } catch (e) {
+    console.log(`Failed to fetch data from Greenhouse`)
+    process.exit(1)
+  }
+
+  console.log(`jobPosts fetched`, jobPosts.length)
+  console.log(`departments fetched`, departments.length)
+  return Promise.all(
+    departments.map(async department => {
+      const convertedDepartment = changeId(department)
+      
+      let jobs
+      try {
+        const jobsForDepartmentResults = await getJobsForDepartment(apiToken, convertedDepartment.id)
+        jobs = jobsForDepartmentResults.data.map(job => changeId(job))
+      } catch (e) {
+        console.log(`Failed to fetch jobs for department.`)
+        process.exit(1)
+      }
+
+      var jobPostsMapping = jobPosts.reduce((map, jobPost) => { 
+        map[jobPost.job_id] = jobPost
+        return map
+      }, {})
+
+      var jobPostsForDepartment = jobs.reduce((arr, job) => {
+        const mappedJobPost = jobPostsMapping[job.id]
+        if (mappedJobPost) {
+          arr.push(mappedJobPost)
+        }
+        return arr
+      }, [])
+
+      convertedDepartment.jobPosts =  jobPostsForDepartment
+      const departmentNode = DepartmentNode(changeId(convertedDepartment))
+
+      jobPostsForDepartment.forEach(jobPost => {
+        const convertedJobPost = changeId(jobPost)
+        const jobPostNode = JobPostNode(convertedJobPost, { 
+          parent: departmentNode.id 
+        })
+        createNode(jobPostNode)
+      })
+      
+      createNode(departmentNode)
+    })
+  )
 }
---
+++ src/nodes.js
@@ -0,0 +1,34 @@
+import createNodeHelpers from 'gatsby-node-helpers'
+
+const {
+  createNodeFactory,
+  generateNodeId,
+  generateTypeName,
+} = createNodeHelpers({
+  typePrefix: `Greenhouse`,
+})
+
+const JOB_POST_TYPE = `JobPost`
+const DEPARTMENT_TYPE = `Department`
+
+export const JobPostNode = createNodeFactory(JOB_POST_TYPE, node => {
+  node.slug = slugify(node.title)
+  return node
+})
+
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
+
+export const DepartmentNode = createNodeFactory(DEPARTMENT_TYPE, node => {
+  node.slug = slugify(node.name)
+  node.children = node.jobPosts.map(jobPost => {
+    return generateNodeId(JOB_POST_TYPE, jobPost.id)
+  })
+  return node
+})
